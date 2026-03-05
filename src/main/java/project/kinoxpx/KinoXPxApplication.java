58b3544a34cc0468f9abd4d724f65957b24afc1b
aljamour <alal0002@stud.net.dk>
2026-03-05 18:43:16 +0100

    Inital commit

---
+++ src/main/java/project/kinoxpx/KinoXPxApplication.java
@@ -0,0 +1,13 @@
+package project.kinoxpx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinoXPxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KinoXPxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/project/kinoxpx/KinoXPxApplicationTests.java
@@ -0,0 +1,13 @@
+package project.kinoxpx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinoXPxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
