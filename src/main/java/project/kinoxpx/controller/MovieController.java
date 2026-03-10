1dd69af9c55d014115eaef4adde9a156decb8e5d
aljamour <alal0002@stud.net.dk>
2026-03-10 12:09:52 +0100

    homecontroller unfinished

--- src/main/java/project/kinoxpx/controller/MovieController.java
+++ src/main/java/project/kinoxpx/controller/MovieController.java
@@ -1,4 +1,17 @@
 package project.kinoxpx.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import project.kinoxpx.service.MovieService;
+
+@RestController
 public class MovieController {
+    private final MovieService movieService;
+
+    public MovieController (MovieService movieService) {
+        this.movieService = movieService;
+    }
+
+    @GetMapping("/")
+
 }
