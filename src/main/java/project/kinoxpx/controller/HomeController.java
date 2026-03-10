b21c7f9ec0c23253c03f83a5cc6789aefbaa5c0e
aljamour <alal0002@stud.net.dk>
2026-03-10 12:21:45 +0100

    homcontroller

--- src/main/java/project/kinoxpx/controller/HomeController.java
+++ src/main/java/project/kinoxpx/controller/HomeController.java
@@ -1,17 +1,16 @@
 package project.kinoxpx.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/KinoXP")
 public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        return
+        return "KinoXP api is running";
     }
 
     // TODO:
